741f97ab0b4a8c90a2a1275206f76e3b43633328
Eric Lewis <ericlewis777@gmail.com>
2022-12-29 14:34:22 -0500

    fix: make it obvious you can drag things around

--- MenuChatGPT/Source/ContentView.swift
+++ MenuChatGPT/Source/ContentView.swift
@@ -5,6 +5,11 @@ struct ContentView: View {
     VStack(spacing: 0) {
       Rectangle().foregroundStyle(Color("DarkGrayColor"))
         .frame(height: 15)
+        .overlay {
+          Capsule().fill(Color.secondary)
+            .frame(width: 95, height: 5)
+            .padding(.top, 8)
+        }
       WebView.shared
     }
     .background(Color("BackgroundColor"))
