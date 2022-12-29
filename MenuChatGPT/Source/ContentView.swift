a8532c421d6a5a087508a141791f6c2ef7c423f8
Eric Lewis <ericlewis777@gmail.com>
2022-12-29 14:42:33 -0500

    grabby thingie

--- MenuChatGPT/Source/ContentView.swift
+++ MenuChatGPT/Source/ContentView.swift
@@ -4,13 +4,15 @@ struct ContentView: View {
   var body: some View {
     VStack(spacing: 0) {
       Rectangle().foregroundStyle(Color("DarkGrayColor"))
+        .frame(height: 13)
+      WebView.shared
+      Rectangle().foregroundStyle(Color("DarkGrayColor"))
         .frame(height: 15)
         .overlay {
           Capsule().fill(Color.secondary)
             .frame(width: 95, height: 5)
-            .padding(.top, 8)
+            .padding(.bottom, 2)
         }
-      WebView.shared
     }
     .background(Color("BackgroundColor"))
   }
